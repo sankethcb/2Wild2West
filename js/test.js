bedc6ca1f3ea25aed05c7903972033fa2148e9f5
Sanketh Bhat (RIT Student) <scb8528@ad.rit.edu>
2018-03-01 10:24:41 -0500

    haha will fk u

--- js/test.js
+++ js/test.js
@@ -1,6 +1,10 @@
 //Create a Pixi Application
 let app = new PIXI.Application({width: 256, height: 256});
 
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
 //Add the canvas that Pixi automatically created for you to the HTML document
 window.onload=function(){
     document.body.appendChild(app.view);
