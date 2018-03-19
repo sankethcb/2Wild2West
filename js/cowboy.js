1bdac1d605f9ff1a10fa9ed3c62625a62284b36a
William Montgomery <willgmontgomery@gmail.com>
2018-03-19 16:26:54 -0400

    Fixed bullet hell bug with 2 players. Start of sprite animation method.

--- js/cowboy.js
+++ js/cowboy.js
@@ -8,98 +8,144 @@ export { Cowboy };
 let maxSpeed = 8;
 let deadzone = 0.15;
 let crossRange = 125;
-let lastState = 0;
+let animationFPS = 30;
 
 class Cowboy {
-    constructor(pNum) {
-        this.sprite = new PIXI.Sprite(PIXI.loader.resources["hat"].texture);
-        this.HP = 3;
-        this.sprite.anchor.set(0.5, 0.5);
-        this.sprite.position.x = 50;
-        this.sprite.position.y = 30;
-        b.addCollisionProperties(this.sprite);
+	constructor(pNum) {
+		//Sprite settings
+		this.sprite = new PIXI.Sprite(PIXI.loader.resources["hat"].texture);
+		this.HP = 3;
+		this.sprite.anchor.set(0.5, 0.5);
+		this.sprite.position.x = 50;
+		this.sprite.position.y = 30;
+		b.addCollisionProperties(this.sprite);
 
-        this.crosshair = new PIXI.Sprite(PIXI.loader.resources['crosshair'].texture);
-        this.crosshair.anchor.set(0.5, 0.5);
+		//Set up crosshair sprite
+		this.crosshair = new PIXI.Sprite(PIXI.loader.resources['crosshair'].texture);
+		this.crosshair.anchor.set(0.5, 0.5);
+		this.sprite.addChild(this.crosshair);
 
-        this.sprite.addChild(this.crosshair);
+		this.playerNum = pNum;
+		this.gamepad = gp[pNum - 1];
 
-        this.playerNum = pNum;
-        this.gamepad = gp[pNum - 1];
+		//Movement vars
+		this.velocity = {};
+		this.velocity.x = 0;
+		this.velocity.y = 0;
 
-        this.velocity = {};
-        this.velocity.x = 0;
-        this.velocity.y = 0;
-        this.fwd = {};
-        this.fwd.x = 0;
-        this.fwd.y = 0;
-    }
+		this.fwd = {}; //The direction the player is currently pointed towards
+		this.fwd.x = 0;
+		this.fwd.y = 0;
 
-    //Update the cowboy object
-    update(delta) {
-        if (delta === undefined) {
-            return;
-        }
+		//Shooting vars
+		this.lastTriggerState = 0; //Last state of the trigger used to shoot
 
-        //Movement
-        this.fwd.x = applyDeadzone(this.gamepad.axes[0], deadzone);
-        this.fwd.y = applyDeadzone(this.gamepad.axes[1], deadzone);
+		//Animation vars
+		this.lastDirection = 'S';
+	}
 
-        this.velocity.x = this.fwd.x * maxSpeed;
-        this.velocity.y = this.fwd.y * maxSpeed;
+	//Update the cowboy object
+	update(delta) {
+		if (delta === undefined) {
+			return;
+		}
 
-        this.sprite.position.x += this.velocity.x * delta;
-        this.sprite.position.y += this.velocity.y * delta;
+		//Movement
+		this.fwd.x = applyDeadzone(this.gamepad.axes[0], deadzone);
+		this.fwd.y = applyDeadzone(this.gamepad.axes[1], deadzone);
 
-        //Crosshair position
-        this.crosshair.position.x = applyDeadzone(this.gamepad.axes[2], deadzone) * crossRange;
+		this.velocity.x = this.fwd.x * maxSpeed;
+		this.velocity.y = this.fwd.y * maxSpeed;
 
-        this.crosshair.position.y = applyDeadzone(this.gamepad.axes[3], deadzone) * crossRange;
+		this.sprite.position.x += this.velocity.x * delta;
+		this.sprite.position.y += this.velocity.y * delta;
 
-        if (this.crosshair.position.x == 0 && this.crosshair.position.y == 0) {
-            this.crosshair.visible = false;
-        } else {
-            this.crosshair.visible = true;
-        }
+		//Crosshair position
+		this.crosshair.position.x = applyDeadzone(this.gamepad.axes[2], deadzone) * crossRange;
 
-        //Shooting
-        if (this.gamepad.buttons[7].value > 0.3 && lastState < 0.3) {
-            this.shoot();
-        }
-        lastState = this.gamepad.buttons[7].value;
-        this.keepInBounds();
-    }
+		this.crosshair.position.y = applyDeadzone(this.gamepad.axes[3], deadzone) * crossRange;
 
-    //Keeps the cowboy in bounds
-    keepInBounds() {
-        let x = this.sprite.position.x;
-        let y = this.sprite.position.y;
-        let spriteHalfWidth = this.sprite.width / 2;
-        let spriteHalfHeight = this.sprite.height / 2;
-        let stageWidth = app.renderer.width;
-        let stageHeight = app.renderer.height;
+		if (this.crosshair.position.x == 0 && this.crosshair.position.y == 0) {
+			this.crosshair.visible = false;
+		} else {
+			this.crosshair.visible = true;
+		}
 
-        if (x - spriteHalfWidth <= 0)
-            this.sprite.position.x = spriteHalfWidth;
+		//Shooting
+		if (this.gamepad.buttons[7].value > 0.3 && this.lastTriggerState < 0.3) {
+			this.shoot();
+		}
+		this.lastTriggerState = this.gamepad.buttons[7].value;
 
-        if (x + spriteHalfWidth >= stageWidth)
-            this.sprite.position.x = stageWidth - spriteHalfWidth;
+		this.animate(); //Update sprite texture
 
-        //Add the same padding that the other bounds have
-        if (y + spriteHalfHeight >= stageHeight - 10)
-            this.sprite.position.y = stageHeight - spriteHalfHeight - 10;
+		this.keepInBounds();
+	}
 
-        if (y - spriteHalfHeight <= 0)
-            this.sprite.position.y = spriteHalfHeight;
-    }
+	animate(){
+		//Find what direction the left stick is in
+		let currDirection = '';
+		let horizontalAxis = '';
+		let verticalAxis = '';
 
-    shoot() {
+		if(applyDeadzone(this.gamepad.axes[1], deadzone) > 0){
+			verticalAxis = 'S';
+		}
+		else{
+			verticalAxis = 'N';
+		}
 
-        let bulletFwd = {};
-        bulletFwd.x = applyDeadzone(this.gamepad.axes[2], deadzone);
-        bulletFwd.y = applyDeadzone(this.gamepad.axes[3], deadzone);
-        let bulletClone = new bullet(this, bulletFwd);
+		if(applyDeadzone(this.gamepad.axes[0], deadzone) > 0){
+			horizontalAxis = 'E';
+		}
+		else{
+			horizontalAxis = 'W';
+		}
 
-        AddBullet(bulletClone);
-    }
+		currDirection = verticalAxis + horizontalAxis;
+
+		if(currDirection == ''){
+			//change sprite to last direction
+		}
+		else{
+			//change sprite to current direction
+		}
+			
+
+
+
+	}
+
+	shoot() {
+
+		let bulletFwd = {};
+		bulletFwd.x = applyDeadzone(this.gamepad.axes[2], deadzone);
+		bulletFwd.y = applyDeadzone(this.gamepad.axes[3], deadzone);
+		let bulletClone = new bullet(this, bulletFwd);
+
+		AddBullet(bulletClone);
+	}
+
+	//Keeps the cowboy in bounds
+	keepInBounds() {
+		let x = this.sprite.position.x;
+		let y = this.sprite.position.y;
+		let spriteHalfWidth = this.sprite.width / 2;
+		let spriteHalfHeight = this.sprite.height / 2;
+		let stageWidth = app.renderer.width;
+		let stageHeight = app.renderer.height;
+
+		if (x - spriteHalfWidth <= 0)
+			this.sprite.position.x = spriteHalfWidth;
+
+		if (x + spriteHalfWidth >= stageWidth)
+			this.sprite.position.x = stageWidth - spriteHalfWidth;
+
+		//Add the same padding that the other bounds have
+		if (y + spriteHalfHeight >= stageHeight - 10)
+			this.sprite.position.y = stageHeight - spriteHalfHeight - 10;
+
+		if (y - spriteHalfHeight <= 0)
+			this.sprite.position.y = spriteHalfHeight;
+	}
 }
