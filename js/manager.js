91589ee6c9a6aa9a7fe1d001ef946cc641d8b2ad
William Montgomery <willgmontgomery@gmail.com>
2018-03-20 15:02:57 -0400

    Added howler.js. Added menu music loop.

--- js/manager.js
+++ js/manager.js
@@ -21,184 +21,208 @@ let gameContainer = new PIXI.Container();
 //Bump.js variable
 let b = new Bump(PIXI);
 
+//Audio variables
+let menuLoop = new Howl({
+	src: ['./audio/menuloop.wav'],
+	loop: true
+});
+
 //Initializes manager itself
 function startManager() {
-    setGamepadConnectionEvents();
-    app.ticker.add(delta => gameLoop(delta))
+	setGamepadConnectionEvents();
+	app.ticker.add(delta => gameLoop(delta))
 
-    if(SKIP_MENU)
-    {
-        state = play;
-        InitGame(2);
-        return;
-    }
+	if(SKIP_MENU)
+	{
+		state = play;
+		InitGame(2);
+		return;
+	}
 
-    InitMenu();
+	InitMenu();
 }
 
 //Wrapper to run the proper loop for the proper game state
 function gameLoop(delta) {
-    state(delta);
+	state(delta);
 }
 
 //The loop when in the play state
 function play(delta) {
-    pollGamepads();
+	pollGamepads();
 
-    for (let i = 0; i < players.length; i++) {
-        players[i].update(delta);
-    }
-    for (let i = 0; i < bulletList.length; i++) {
-        bulletList[i].move(delta);
+	for (let i = 0; i < players.length; i++) {
+		players[i].update(delta);
+	}
+	for (let i = 0; i < bulletList.length; i++) {
+		bulletList[i].move(delta);
 
-    }
-    Collisions();
+	}
+	Collisions();
 }
 
 //The loop when at the menu
 function menu(delta) {
-    controlText.text = "Controllers Connected: " + numPads;
+	controlText.text = "Controllers Connected: " + numPads;
 }
 
 
-function SwitchState() {
-    if (state == menu) {
-        //Don't allow the game to start if there are no gamepads
-        if (numPads == 0) {
-            let warnText = new PIXI.Text('Please connect at least 1 controller to play.');
-            warnText.position.set(controlText.x, controlText.y + 50);
-            menuContainer.addChild(warnText);
-            return;
-        }
+function SwitchState(nextState) {
+	console.log(nextState);
+	switch(nextState){
+		case play:
+			//Don't allow the game to start if there are no gamepads
+			if (numPads == 0) {
+				let warnText = new PIXI.Text('Please connect at least 1 controller to play.');
+				warnText.position.set(controlText.x, controlText.y + 50);
+				menuContainer.addChild(warnText);
+				return;
+			}
 
-        InitGame(numPads > 2 ? 2 : numPads);
-        app.stage.removeChild(menuContainer);
-        app.stage.addChild(gameContainer);
-        state = play;
+			InitGame(numPads > 2 ? 2 : numPads);
+			clearCanvas();
+			app.stage.addChild(gameContainer);
+			state = play;
+			break;
+		case menu:
+			clearCanvas();
+			app.stage.addChild(menuContainer);
+			menuLoop.play();
+			state = menu;
+			break;
+					}
+}
 
-    } else if (state == play) {
-        app.stage.removeChild(gameContainer);
-        app.stage.addChild(menuContainer);
-        state = menu;
-    }
+//Clears all containers off the canvas and stops any music
+function clearCanvas(){
+	app.stage.removeChild(menuContainer);
+	app.stage.removeChild(gameContainer);
+
+	menuLoop.stop();
 }
 
 //Start up the main menu with all graphics
 function InitMenu() {
-    //Title Message
-    let style = new PIXI.TextStyle({
-        fontFamily: "Tahoma",
-        fontSize: 100,
-        fill: "black",
-        stroke: 'white',
-        strokeThickness: 4,
-        dropShadow: true,
-        dropShadowColor: "#000000",
-        dropShadowBlur: 4,
-        dropShadowAngle: Math.PI / 6,
-        dropShadowDistance: 6,
-    });
+	//Title Message
+	let style = new PIXI.TextStyle({
+		fontFamily: "Tahoma",
+		fontSize: 100,
+		fill: "black",
+		stroke: 'white',
+		strokeThickness: 4,
+		dropShadow: true,
+		dropShadowColor: "#000000",
+		dropShadowBlur: 4,
+		dropShadowAngle: Math.PI / 6,
+		dropShadowDistance: 6,
+	});
 
-    let titleText = new PIXI.Text("2 Wild 2 West", style);
-    titleText.anchor.set(0.5);
-    titleText.position.set(app.renderer.width / 2, 400);
-    menuContainer.addChild(titleText);
+	let titleText = new PIXI.Text("2 Wild 2 West", style);
+	titleText.anchor.set(0.5);
+	titleText.position.set(app.renderer.width / 2, 400);
+	menuContainer.addChild(titleText);
 
-    //Controller Status
-    controlText = new PIXI.Text("Controllers Connected: " + numPads);
-    controlText.position.set(100, app.renderer.height - (app.renderer.height / 6));
-    app.stage.addChild(controlText);
-    menuContainer.addChild(controlText);
+	//Controller Status
+	controlText = new PIXI.Text("Controllers Connected: " + numPads);
+	controlText.position.set(100, app.renderer.height - (app.renderer.height / 6));
+	app.stage.addChild(controlText);
+	menuContainer.addChild(controlText);
 
-    //Star Sprite
-    let button = new PIXI.Sprite(PIXI.loader.resources["star"].texture);
-    button.scale.x = 0.2;
-    button.scale.y = 0.2;
-    button.x = -button.width / 2;
-    button.y = -110;
+	//Star Sprite
+	let button = new PIXI.Sprite(PIXI.loader.resources["star"].texture);
+	button.scale.x = 0.2;
+	button.scale.y = 0.2;
+	button.x = -button.width / 2;
+	button.y = -110;
 
-    //Button Text
-    style.fontSize = 50;
-    let buttonMessage = new PIXI.Text("Start", style);
-    buttonMessage.anchor.set(0.5);
+	//Button Text
+	style.fontSize = 50;
+	let buttonMessage = new PIXI.Text("Start", style);
+	buttonMessage.anchor.set(0.5);
 
-    //Add to container and positioning
-    let buttonContainer = new PIXI.Container();
-    buttonContainer.addChild(button);
-    buttonContainer.addChild(buttonMessage);
-    buttonContainer.x = app.renderer.width / 2;
-    buttonContainer.y = 600;
+	//Add to container and positioning
+	let buttonContainer = new PIXI.Container();
+	buttonContainer.addChild(button);
+	buttonContainer.addChild(buttonMessage);
+	buttonContainer.x = app.renderer.width / 2;
+	buttonContainer.y = 600;
 
-    //Make it interactable
-    buttonContainer.interactive = true;
-    buttonContainer.buttonMode = true;
-    buttonContainer.on('pointerdown', SwitchState);
+	//Make it interactable
+	buttonContainer.interactive = true;
+	buttonContainer.buttonMode = true;
+	buttonContainer.on('pointerdown', (event) => {
+		SwitchState(play);
+	});
 
-    menuContainer.addChild(buttonContainer);
+	menuContainer.addChild(buttonContainer);
 
-    app.stage.addChild(menuContainer);
+	app.stage.addChild(menuContainer);
+
+	menuLoop.once('load', function(){
+		menuLoop.play();
+	});	
 }
 
 //Start up the core game when in the play state
 function InitGame(numPlayers) {
-    if (numPlayers == 0) {
-        return;
-    }
+	if (numPlayers == 0) {
+		return;
+	}
 
-    //Init all players
-    for (let i = 0; i < numPlayers; i++) {
-        players[i] = new Cowboy(i + 1);
-        gameContainer.addChild(players[i].sprite);
-    }
+	//Init all players
+	for (let i = 0; i < numPlayers; i++) {
+		players[i] = new Cowboy(i + 1);
+		gameContainer.addChild(players[i].sprite);
+	}
 }
 
 //Add a bullet to the bullet list
 function AddBullet(bullet) {
-    bulletList.push(bullet);
-    gameContainer.addChild(bullet.sprite);
+	bulletList.push(bullet);
+	gameContainer.addChild(bullet.sprite);
 }
 
 function RemoveBullet(bullet) {
-    let index = bulletList.indexOf(bullet);
-    if (index != -1) {
-        gameContainer.removeChild(bullet.sprite);
-        bulletList.splice(index, 1);
-        bullet = null;
-    }
+	let index = bulletList.indexOf(bullet);
+	if (index != -1) {
+		gameContainer.removeChild(bullet.sprite);
+		bulletList.splice(index, 1);
+		bullet = null;
+	}
 }
 
 
 //Game Collisions
 function Collisions() {
 
-    if (players[1] != null) {
-        //Player - Player intersection
-        b.hit(players[0].sprite, players[1].sprite, true);
-        b.hit(players[1].sprite, players[0].sprite, true);
-        //Player - map intersection
-        b.hit(players[1].sprite, mapList, true);
-    }
-    //Player - map intersection
-    b.hit(players[0].sprite, mapList, true);
+	if (players[1] != null) {
+		//Player - Player intersection
+		b.hit(players[0].sprite, players[1].sprite, true);
+		b.hit(players[1].sprite, players[0].sprite, true);
+		//Player - map intersection
+		b.hit(players[1].sprite, mapList, true);
+	}
+	//Player - map intersection
+	b.hit(players[0].sprite, mapList, true);
 
 
 
 
-    //Player  - bullet collisions
-    for (var i = 0; i < bulletList.length; i++) {
-        if (bulletList[i].playerNum == 2) {
-            if (b.hit(players[0].sprite, bulletList[i].sprite)) {
-                RemoveBullet(bulletList[i]);
-                players[0].HP--;
-            }
-        } else if (bulletList[i].playerNum == 1) {
-            if (players[1] != null)
-                if (b.hit(players[1].sprite, bulletList[i].sprite)) {
-                    RemoveBullet(bulletList[i]);
-                    players[1].HP--;
-                }
-        }
-    }
+	//Player  - bullet collisions
+	for (var i = 0; i < bulletList.length; i++) {
+		if (bulletList[i].playerNum == 2) {
+			if (b.hit(players[0].sprite, bulletList[i].sprite)) {
+				RemoveBullet(bulletList[i]);
+				players[0].HP--;
+			}
+		} else if (bulletList[i].playerNum == 1) {
+			if (players[1] != null)
+				if (b.hit(players[1].sprite, bulletList[i].sprite)) {
+					RemoveBullet(bulletList[i]);
+					players[1].HP--;
+				}
+		}
+	}
 
 
 
